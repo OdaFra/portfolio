5b814924878e4d64c00a542a8f5e88cfb0bf3235
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-07-01 15:43:41 -0400

    feat: Agregamos flutter_speed_dial para crear un floatingActionButton customizado.

---
+++ lib/src/widgets/floating_action_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:web_portfolio/src/constants/colors.dart';
+
+class CustomFloatActionButton extends StatelessWidget {
+  const CustomFloatActionButton(
+      {super.key, this.onLanguageTap, this.onModoDarkTap});
+  final VoidCallback? onLanguageTap;
+  final VoidCallback? onModoDarkTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      animatedIconTheme: const IconThemeData(size: 25.0),
+      backgroundColor: CustomColor.yellowPrimary,
+      visible: true,
+      curve: Curves.bounceInOut,
+      children: [
+        SpeedDialChild(
+          child: const Icon(
+            Icons.dark_mode_outlined,
+            color: CustomColor.whitePrimary,
+          ),
+          backgroundColor: CustomColor.yellowPrimary,
+          onTap: onLanguageTap,
+          label: 'English',
+          labelStyle: const TextStyle(
+              fontWeight: FontWeight.w500, color: CustomColor.scaffoldBg),
+          labelBackgroundColor: CustomColor.whitePrimary,
+        ),
+        SpeedDialChild(
+          child: const Icon(
+            Icons.language,
+            color: CustomColor.whitePrimary,
+          ),
+          backgroundColor: CustomColor.yellowPrimary,
+          onTap: onLanguageTap,
+          label: 'Theme',
+          labelStyle: const TextStyle(
+              fontWeight: FontWeight.w500, color: CustomColor.scaffoldBg),
+          labelBackgroundColor: CustomColor.whitePrimary,
+        ),
+      ],
+    );
+  }
+}
