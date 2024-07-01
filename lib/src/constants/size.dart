29e0c8f2ce1067751c128da542f74637c3791068
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-07-01 12:28:13 -0400

    Actualizamos el archivo .gitignore

--- lib/src/constants/size.dart
+++ lib/src/constants/size.dart
@@ -1,1 +1,7 @@
 const double kMinDestopWith = 700.0;
+
+// extension kSize on MediaQueryData {
+//   bool get isMobile => size.width <= 700;
+//   bool get isTable => size.width <= 1100 && size.width > 700;
+//   bool get isDektop => size.width > 1100;
+// }
