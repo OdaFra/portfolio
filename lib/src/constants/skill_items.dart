873c463b29ec134e8fae21eec6883354baeb0694
Oscar Ramirez <oscar.ramirez@tigo.net.py>
2024-07-02 12:56:51 -0400

    feat: Agregamos un archivo constante para los skills

---
+++ lib/src/constants/skill_items.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+List<Map> platformItems = [
+  {
+    "icon": const Icon(Icons.android),
+    "title": "Android Dev",
+  },
+  {
+    "icon": const Icon(Icons.apple),
+    "title": "IOS Dev",
+  },
+  {
+    "icon": const Icon(Icons.computer),
+    "title": "Backend Dev",
+  }
+];
+
+const List<Map> skillItems = [
+  {
+    "icon": Icon(Icons.flutter_dash),
+    "title": "Dart",
+  },
+  {
+    "icon": Icon(Icons.flutter_dash),
+    "title": "Flutter",
+  },
+  {
+    "icon": Icon(Icons.computer),
+    "title": "Backend Dev",
+  }
+];
