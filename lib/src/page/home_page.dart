85fc46baa18402ff794a4032d758ac40d1d57b51
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-07-08 20:44:59 -0400

    fix: Agregamos las secciones desarrolladas anteriormente para confirma avances de la vista home.

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: sized_box_for_whitespace
 
+
 import 'package:flutter/material.dart';
 
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:web_portfolio/src/constants/constants.dart';
-import 'package:web_portfolio/src/utils/project_utils.dart';
 import 'package:web_portfolio/src/widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -32,104 +32,50 @@ class _HomePageState extends State<HomePage> {
         body: ListView(
           scrollDirection: Axis.vertical,
           children: [
-            // //Main
-            // if (constraints.maxWidth >= kMinDestopWith)
-            //   const HeaderWeb()
-            // else
-            //   HeaderMobile(
-            //     onLogoTap: () {},
-            //     onMenuTap: () {
-            //       scaffoldKey.currentState?.openEndDrawer();
-            //     },
-            //   ),
-            // //Presentation
-            // if (constraints.maxWidth >= kMinDestopWith)
-            //   const MainDesktop()
-            // else
-            //   const MainMobile(),
-            // //Skill
-            // Container(
-            //   width: screenWidth,
-            //   padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
-            //   color: CustomColor.bgLiht1,
-            //   child: Column(
-            //     mainAxisSize: MainAxisSize.min,
-            //     children: [
-            //       //title
-            //       const AutoSizeText(
-            //         'What I can do',
-            //         style: TextStyle(
-            //           fontSize: 24,
-            //           fontWeight: FontWeight.bold,
-            //           color: CustomColor.whitePrimary,
-            //         ),
-            //       ),
-            //       const SizedBox(height: 50),
-            //       //Platforms and Skill
-            //       if (constraints.maxWidth >= kMedDestopWith)
-            //         const SkillDesktop()
-            //       else
-            //         const SkillMobile()
-            //     ],
-            //   ),
-            // ),
-
-            //Work Project
+            //Main
+            if (constraints.maxWidth >= kMinDestopWith)
+              const HeaderWeb()
+            else
+              HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+              ),
+            //Presentation
+            if (constraints.maxWidth >= kMinDestopWith)
+              const MainDesktop()
+            else
+              const MainMobile(),
+            //Skill
             Container(
               width: screenWidth,
               padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
+              color: CustomColor.bgLiht1,
               child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  //Work Project title
+                  // title
                   const AutoSizeText(
-                    'Work projects',
+                    'What I can do',
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                       color: CustomColor.whitePrimary,
                     ),
                   ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Wrap(
-                    spacing: 25,
-                    runSpacing: 25,
-                    children: [
-                      for (int i = 0; i < workProjectUtils.length; i++)
-                        ProjectsCardWidget(
-                          project: workProjectUtils[i],
-                        ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  //Hobby Project
-                  const AutoSizeText(
-                    'Hobby projects',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: CustomColor.whitePrimary,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Wrap(
-                    spacing: 25,
-                    runSpacing: 25,
-                    children: [
-                      for (int i = 0; i < hobbyProjectUtils.length; i++)
-                        ProjectsCardWidget(
-                          project: hobbyProjectUtils[i],
-                        ),
-                    ],
-                  ),
+                  const SizedBox(height: 50),
+                  //Platforms and Skill
+                  if (constraints.maxWidth >= kMedDestopWith)
+                    const SkillDesktop()
+                  else
+                    const SkillMobile()
                 ],
               ),
             ),
+            const SizedBox(height: 30),
+            // PROJECT
+            ProjectSection(screenWidth: screenWidth),
 
             //Contact
             Container(
--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -5,6 +5,7 @@ export 'main_desktop.dart';
 export 'main_mobile.dart';
 export 'mobile_drawer.dart';
 export 'project_card.dart';
+export 'project_section.dart';
 export 'site_logo.dart';
 export 'skill_desktop.dart';
 export 'skill_mobile.dart';
