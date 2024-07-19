cd87e8790d2bc9516412f4ec572fe4513f790a36
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-07-19 11:35:25 -0400

    feat: Agregamos widget de FooterSection customizado.

---
+++ lib/src/widgets/footer_section.dart
@@ -0,0 +1,24 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/constants.dart';
+
+class FooterSection extends StatelessWidget {
+  const FooterSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      width: double.maxFinite,
+      alignment: Alignment.center,
+      child: const AutoSizeText(
+        'Made by Oscar Ramirez with flutter 3.22',
+        style: TextStyle(
+          fontWeight: FontWeight.w400,
+          color: CustomColor.witheSecondary,
+        ),
+      ),
+    );
+  }
+}
