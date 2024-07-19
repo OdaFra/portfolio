25bf938bd757c10dd5703911d870ec84f20e63a9
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-07-19 11:34:11 -0400

    fix: Eliminamos comentarios

--- lib/src/constants/size.dart
+++ lib/src/constants/size.dart
@@ -1,8 +1,2 @@
 const double kMinDestopWith = 750.0;
 const double kMedDestopWith = 850.0;
-
-// extension kSize on MediaQueryData {
-//   bool get isMobile => size.width <= 700;
-//   bool get isTable => size.width <= 1100 && size.width > 700;
-//   bool get isDektop => size.width > 1100;
-// }
