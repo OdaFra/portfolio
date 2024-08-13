5a283bc8b2dff6392e140a812b257677f8bed196
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-13 17:34:51 -0400

    refactor: Aplicamos refactorizacion al widgtes customizado de floatActionButton

--- lib/src/widgets/floating_action_button.dart
+++ lib/src/widgets/floating_action_button.dart
@@ -1,49 +1,23 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:web_portfolio/src/themes/colors.dart';
 
 class CustomFloatActionButton extends StatelessWidget {
-  const CustomFloatActionButton(
-      {super.key, this.onLanguageTap, this.onModoDarkTap});
-  final VoidCallback? onLanguageTap;
-  final Function()? onModoDarkTap;
+  CustomFloatActionButton({super.key, required this.onPress});
+  void Function()? onPress;
 
   @override
   Widget build(BuildContext context) {
     return SpeedDial(
-      
-      animatedIcon: AnimatedIcons.menu_close,
+      onPress: onPress,
+      icon: Icons.keyboard_control_key,
       animatedIconTheme: const IconThemeData(size: 25.0),
       backgroundColor: CustomColor.yellowPrimary,
       visible: true,
       curve: Curves.bounceInOut,
-      children: [
-        SpeedDialChild(
-          child: const Icon(
-            Icons.dark_mode_outlined,
-            color: CustomColor.whitePrimary,
-          ),
-          backgroundColor: CustomColor.yellowPrimary,
-          onTap: onModoDarkTap,
-          label: 'Theme',
-          labelStyle: const TextStyle(
-              fontWeight: FontWeight.w500, color: CustomColor.scaffoldBg),
-          labelBackgroundColor: CustomColor.whitePrimary,
-        ),
-        SpeedDialChild(
-          child: const Icon(
-            Icons.language,
-            color: CustomColor.whitePrimary,
-          ),
-          backgroundColor: CustomColor.yellowPrimary,
-          onTap: onLanguageTap,
-          label: 'English',
-          labelStyle: const TextStyle(
-              fontWeight: FontWeight.w500, color: CustomColor.scaffoldBg),
-          labelBackgroundColor: CustomColor.whitePrimary,
-        ),
-      ],
     );
   }
 }
