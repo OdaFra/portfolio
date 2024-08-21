4a17b5636376659e7de981879668d277f61ee3f1
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-20 21:36:27 -0400

    feat: Agregamos una lista de contacts items como archivo constante.

---
+++ lib/src/constants/contacts_items.dart
@@ -0,0 +1,14 @@
+List<Map> contactsItems = [
+  {
+    "icon": 'assets/contacts/linkedin.png',
+    "title": "Linkedin",
+  },
+  {
+    "icon": 'assets/contacts/github.png',
+    "title": "GitHub",
+  },
+  {
+    "icon": 'assets/contact/email.png',
+    "title": "osramirezf@gmail.com",
+  },
+];
