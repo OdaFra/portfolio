96614e324ae40dc996155a5ce9af6fdc01fa9868
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-20 21:39:50 -0400

    feat: Agregamos email

--- lib/src/constants/sns_link.dart
+++ lib/src/constants/sns_link.dart
@@ -1,5 +1,6 @@
 class SnsLinks {
   static const String github = 'https://github.com/OdaFra';
+  static const String email = 'osramirezf@gmail.com';
   static const String linkedin =
       'https://www.linkedin.com/in/oscar-ramirez-franco/';
 }
