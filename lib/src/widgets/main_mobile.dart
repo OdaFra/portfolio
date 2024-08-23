a84a7a22eefab375a30793b1e79675529eed0297
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-22 20:01:01 -0400

    refactor: Aplicamos refactor para main en vista mobile tanto en imagen, texto y botones a visualizar

--- lib/src/widgets/main_mobile.dart
+++ lib/src/widgets/main_mobile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:web_portfolio/src/widgets/widgets.dart';
 
 import '../themes/themes.dart';
 
@@ -20,12 +21,12 @@ class MainMobile extends StatelessWidget {
           SizedBox(height: (screenHeight * 0.15) / 2),
           CircleAvatar(
             backgroundColor: const Color.fromRGBO(0, 0, 0, 0),
-            radius: (screenWidth * 0.30) / 1.6,
+            radius: (screenWidth * 0.35) / 2.5,
             child: ShaderMask(
               shaderCallback: (bounds) {
                 return LinearGradient(colors: [
-                  CustomColor.scaffoldBg.withOpacity(0.6),
-                  CustomColor.scaffoldBg.withOpacity(0.6)
+                  CustomColor.scaffoldBg.withOpacity(0.5),
+                  CustomColor.scaffoldBg.withOpacity(0.5)
                 ]).createShader(bounds);
               },
               blendMode: BlendMode.srcATop,
@@ -37,36 +38,38 @@ class MainMobile extends StatelessWidget {
               ),
             ),
           ),
-          const SizedBox(height: 30),
-          const AutoSizeText(
-            "Hi, \nI'm Oscar Ramirez\n A Software Engineer",
-            style: TextStyle(
-              color: CustomColor.witheSecondary,
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0,
-              height: 1.5,
-            ),
-          ),
-          const SizedBox(height: 15),
-          Flexible(
-            child: SizedBox(
-              width: 180,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: CustomColor.yellowPrimary,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 25),
+              const AutoSizeText(
+                "Hey, soy Oscar Ramirez",
+                style: TextStyle(
+                  color: CustomColor.whitePrimary,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 16.0,
+                  height: 1.5,
+                  letterSpacing: 3,
                 ),
-                onPressed: () {},
-                child: const AutoSizeText(
-                  'Get in touch',
-                  style: TextStyle(
-                    color: CustomColor.whitePrimary,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
+              ),
+              const SizedBox(height: 20),
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 350),
+                child: const Center(
+                  child: AutoSizeText(
+                    "+3 años de experiencia como desarrollador, obteniendo un sólido conocimiento técnico y conceptual del ciclo de vida de desarrollo de software, como la implementación de arquitectura limpia.",
+                    style: TextStyle(
+                      color: CustomColor.textFieldBg,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.0,
+                    ),
                   ),
                 ),
               ),
-            ),
+              const SizedBox(height: 20),
+              const CustomButtonContacts()
+            ],
           )
         ],
       ),
