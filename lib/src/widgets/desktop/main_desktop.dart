a2c9c7e814c4ee3ac89e3dedb992f4bd22682ccb
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-08-23 15:46:32 -0400

    refactor: Adecuamos widget segun CustomPresentationAM

--- lib/src/widgets/desktop/main_desktop.dart
+++ lib/src/widgets/desktop/main_desktop.dart
@@ -1,7 +1,4 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-
-import '../../themes/colors.dart';
 import '../widgets.dart';
 
 class MainDesktop extends StatelessWidget {
@@ -28,10 +25,7 @@ class MainDesktop extends StatelessWidget {
                 backgroundColor: Colors.transparent,
                 radius: (screenWidth * 0.10) / 1.7,
                 child: ClipOval(
-                  child: Image.asset(
-                    'assets/imgs/perfilgithub.jpg',
-                    // width: (size.width * 0.5) / 2,
-                  ),
+                  child: Image.asset('assets/imgs/perfilgithub.jpg'),
                 ),
               ),
               const SizedBox(width: 30),
@@ -40,40 +34,7 @@ class MainDesktop extends StatelessWidget {
                 child: const Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    AutoSizeText(
-                      "Hey, soy Oscar Ramirez",
-                      style: TextStyle(
-                        color: CustomColor.whitePrimary,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 26.0,
-                        height: 1.5,
-                        letterSpacing: 3,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    AutoSizeText(
-                      "+3 años de experiencia como desarrollador, obteniendo un sólido conocimiento técnico y conceptual del ciclo de vida de desarrollo de software, como la implementación de arquitectura limpia.",
-                      style: TextStyle(
-                        color: CustomColor.textFieldBg,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    SizedBox(height: 25),
-                    AutoSizeText(
-                      "Contáctame",
-                      style: TextStyle(
-                        color: CustomColor.whitePrimary,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14.0,
-                        height: 1.5,
-                        letterSpacing: 3,
-                      ),
-                    ),
-                    Divider(
-                      color: CustomColor.grey4Color,
-                    ),
-                    SizedBox(height: 10),
+                    CustomPresentationAM(maxWidth: 450),
                     CustomButtonContacts(),
                   ],
                 ),
