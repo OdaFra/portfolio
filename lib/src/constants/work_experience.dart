d6a53312a44003cb4e4210f4ae15d6f7a236f77e
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-09-03 18:43:39 -0400

    feat: Agregamos lista en base a la experiencia profesional

---
+++ lib/src/constants/work_experience.dart
@@ -0,0 +1,34 @@
+List<Map> workExperienceItems = [
+  {
+    "company": 'Millicom (Tigo)',
+    "position": 'Mobile Software Engineer | Flutter',
+    "year": "Abr. 2024 - Actualidad",
+    "ubication": 'En remoto - Paraguay',
+    "description":
+        'Actualmente contribuyó en el desarrollo de la App de Tigo Money, la cual se encuentra disponible en los siguientes paises: Paraguay, Bolivia, Honduras, Guatemala y El Salvador. Aplicando los principios de Clean Architecture para mantener una estructura de código limpio, modular y fácil de mantener.'
+  },
+  {
+    "company": 'Tigo Paraguay',
+    "position": 'Apis Development Engineer',
+    "year": "Abr. 2023 - May. 2024",
+    "ubication": 'Fernando de la Mora, Zona Sur - Paraguay',
+    "description":
+        'Partícipe y desarrollé diferentes proyectos de integración, procesos, creación, modificación y mantenimiento de distintos Web Services como Apis utilizando Oracle Fusion Middlewares.'
+  },
+  {
+    "company": 'Freelance · Profesional independiente',
+    "position": 'Mobile Software Developer',
+    "year": "Mar. 2022 - Actualidad",
+    "ubication": 'Asunción - Paraguay',
+    "description":
+        'Con más de 2 años de experiencia como desarrollador, obteniendo un sólido conocimiento técnico y conceptual del ciclo de vida de desarrollo de software, como la implementación de arquitectura limpia y patrones MVC y MVVM.'
+  },
+  {
+    "company": 'ITTI Digital',
+    "position": 'Mobile Software Developer',
+    "year": "Abr. 2022 - Abr. 2023",
+    "ubication": 'Asunción - Paraguay',
+    "description":
+        'Desarrolé aplicaciones móviles con Flutter/ Dart para el sector financiendo, donde integré servicios Api Rest, base de datos, reporte y seguimiento de tareas e implemente principios de arquitectura limpia para garantizar un código eficiente, modular y fácil de mantener.'
+  },
+];
