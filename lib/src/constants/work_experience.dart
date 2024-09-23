0f811ce3e0d133029e72fc410f7e150d4a9af3dd
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-09-23 14:21:11 -0400

    fix: Agregamos las secciones de skill faltantes para experiencia laboral con sus respectivas imagenes

--- lib/src/constants/work_experience.dart
+++ lib/src/constants/work_experience.dart
@@ -24,7 +24,16 @@ List<Map> workExperienceItems = [
     "year": "Abr. 2023 - May. 2024",
     "ubication": 'Fernando de la Mora, Zona Sur - Paraguay',
     "description":
-        'Partícipe y desarrollé diferentes proyectos de integración, procesos, creación, modificación y mantenimiento de distintos Web Services como Apis utilizando Oracle Fusion Middlewares.'
+        'Partícipe y desarrollé diferentes proyectos de integración, procesos, creación, modificación y mantenimiento de distintos Web Services como Apis utilizando Oracle Fusion Middlewares.',
+    'skill': {
+      'jdev': 'imgs/tools/jdev.png',
+      'osb': 'imgs/tools/osb.png',
+      'soapui': 'imgs/tools/soapui.png',
+      'kibana': 'imgs/tools/kibana.png',
+      "git": 'imgs/tools/git.png',
+      "gitlab": 'imgs/tools/gitlab.png',
+      "postman": 'imgs/tools/postman.png',
+    }
   },
   {
     "company": 'Freelance · Profesional independiente',
@@ -32,7 +41,23 @@ List<Map> workExperienceItems = [
     "year": "Mar. 2022 - Actualidad",
     "ubication": 'Asunción - Paraguay',
     "description":
-        'Con más de 2 años de experiencia como desarrollador, obteniendo un sólido conocimiento técnico y conceptual del ciclo de vida de desarrollo de software, como la implementación de arquitectura limpia y patrones MVC y MVVM.'
+        'Con más de 2 años de experiencia como desarrollador, obteniendo un sólido conocimiento técnico y conceptual del ciclo de vida de desarrollo de software, como la implementación de arquitectura limpia y patrones MVC y MVVM.',
+    'skill': {
+      "dart": 'imgs/languages/Dart.png',
+      "flutter": 'imgs/framework/Flutter.png',
+      "python": 'imgs/languages/Python.png',
+      "fastapi": 'imgs/framework/fastapi.png',
+      "django": 'imgs/framework/Django.png',
+      "docker": 'imgs/tools/docker.png',
+      "nodejs": 'imgs/tools/nodeJs.png',
+      "express": 'imgs/framework/expressjs.png',
+      "ts": 'imgs/languages/typescript.png',
+      "js": 'imgs/languages/Js.png',
+      "git": 'imgs/tools/git.png',
+      "github": 'imgs/tools/github.png',
+      "postman": 'imgs/tools/postman.png',
+      "vscode": 'imgs/tools/vscode.png',
+    }
   },
   {
     "company": 'ITTI Digital',
@@ -40,6 +65,14 @@ List<Map> workExperienceItems = [
     "year": "Abr. 2022 - Abr. 2023",
     "ubication": 'Asunción - Paraguay',
     "description":
-        'Desarrolé aplicaciones móviles con Flutter/ Dart para el sector financiendo, donde integré servicios Api Rest, base de datos, reporte y seguimiento de tareas e implemente principios de arquitectura limpia para garantizar un código eficiente, modular y fácil de mantener.'
+        'Desarrolé aplicaciones móviles con Flutter/ Dart para el sector financiendo, donde integré servicios Api Rest, base de datos, reporte y seguimiento de tareas e implemente principios de arquitectura limpia para garantizar un código eficiente, modular y fácil de mantener.',
+    'skill': {
+      "dart": 'imgs/languages/Dart.png',
+      "flutter": 'imgs/framework/Flutter.png',
+      "git": 'imgs/tools/git.png',
+      "github": 'imgs/tools/github.png',
+      "postman": 'imgs/tools/postman.png',
+      "vscode": 'imgs/tools/vscode.png',
+    }
   },
 ];
