4bd6ef5a945c41b3ab8ec5aa2e57d0ee6c04a50a
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-10-07 08:20:40 -0300

    fix: Adecuamos la seccion para recibir la lista sobre aboutme en el body

--- lib/src/widgets/presentation_am.dart
+++ lib/src/widgets/presentation_am.dart
@@ -7,46 +7,55 @@ class CustomPresentationAM extends StatelessWidget {
   const CustomPresentationAM({
     super.key,
     required this.maxWidth,
+    required this.aboutMe,
   });
 
   final double maxWidth;
+  final List<Map> aboutMe;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: maxWidth <= 350
-              ? MainAxisAlignment.center
-              : MainAxisAlignment.start,
-          children: [
-            Flexible(
-              child: AutoSizeText(
-                "Hey, soy Oscar Ramirez",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  color: CustomColor.whitePrimary,
-                  fontWeight: FontWeight.w800,
-                  fontSize: maxWidth <= 350 ? 16.0 : 24.0,
-                  height: 1.5,
-                  letterSpacing: 1,
+        for (int i = 0; i < aboutMe.length; i++) ...[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Column(
+              crossAxisAlignment: maxWidth <= 300
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  aboutMe[i]['me'],
+                  textAlign:
+                      maxWidth <= 300 ? TextAlign.center : TextAlign.start,
+                  style: TextStyle(
+                    color: CustomColor.whitePrimary,
+                    fontWeight: FontWeight.w800,
+                    fontSize: maxWidth <= 350 ? 16.0 : 24.0,
+                    height: 1.5,
+                    letterSpacing: 1,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 20),
+                AutoSizeText(
+                  aboutMe[i]['presentation'],
+                  textAlign:
+                      maxWidth <= 300 ? TextAlign.center : TextAlign.start,
+                  style: TextStyle(
+                    color: CustomColor.textFieldBg,
+                    fontWeight: FontWeight.bold,
+                    fontSize: maxWidth <= 350 ? 14 : 16.0,
+                  ),
+                  maxLines: 6,
+                ),
+              ],
             ),
-          ],
-        ),
-        const SizedBox(height: 20),
-        AutoSizeText(
-          textAlign: maxWidth <= 350 ? TextAlign.center : TextAlign.start,
-          "+4 años de experiencia. Ingeniero en Informático de Asuncion, Paraguay 🇵🇾. Especializado en el desarrollo de aplicaciones móviles como el Back-End obteniendo un sólido conocimiento técnico y conceptual del ciclo de vida de desarrollo de software, como la implementación de arquitectura limpia.",
-          style: TextStyle(
-            color: CustomColor.textFieldBg,
-            fontWeight: FontWeight.bold,
-            fontSize: maxWidth <= 350 ? 14 : 16.0,
           ),
-        ),
-        const SizedBox(height: 25),
+        ],
+
+        // El resto del contenido fijo
         AutoSizeText(
           "Contáctame",
           style: TextStyle(
