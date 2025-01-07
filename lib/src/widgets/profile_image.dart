20b97533c6dd07b2dec1584143ecb277f6328bf2
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-01-07 09:52:44 -0300

    feat: Agregamos archivo de profile image para centralizar el widget

---
+++ lib/src/widgets/profile_image.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ProfileImage extends StatelessWidget {
+  final String imagePath;
+  final double height;
+  final double width;
+  final double blurRadius;
+  final double spreadRadius;
+
+  const ProfileImage({
+    super.key,
+    required this.imagePath,
+    required this.height,
+    required this.width,
+    this.blurRadius = 6.0, 
+    this.spreadRadius = 2.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: spreadRadius,
+            blurRadius: blurRadius,
+            offset: const Offset(4, 4),
+          ),
+        ],
+      ),
+      child: Image.asset(
+        imagePath,
+        fit: BoxFit.cover,
+        frameBuilder: (BuildContext context, Widget child, int? frame, bool wasSynchronouslyLoaded) {
+          if (wasSynchronouslyLoaded) {
+            return child;
+          }
+          return AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            child: frame != null
+                ? child
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+          );
+        },
+      ),
+    );
+  }
+}
